c3aedac40d2b38db2714596d171813948744f665
Brian Takita <brian.takita@gmail.com>
2021-11-16 21:22:55 -0600

    formatting

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,3 @@
 import { throw_missing_env } from '@ctx-core/env'
-const SESSION_KEY =
-	process.env.SESSION_KEY
-	|| throw_missing_env('SESSION_KEY')
+const SESSION_KEY = process.env.SESSION_KEY || throw_missing_env('SESSION_KEY')
 export { SESSION_KEY }

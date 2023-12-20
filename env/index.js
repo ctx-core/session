2127f9397493a34038275cd10e32348d0feed025
Brian Takita <brian.takita@gmail.com>
2023-12-20 04:33:20 -0500

    consolidate dependencies

--- env/index.js
+++ env/index.js
@@ -1,4 +1,4 @@
-import { import_meta_env_, missing_env__throw } from '@ctx-core/env'
+import { import_meta_env_, missing_env__throw } from 'ctx-core/env'
 export const SESSION_KEY =
 	import_meta_env_().SESSION_KEY
 	|| missing_env__throw('SESSION_KEY')
